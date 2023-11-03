82e8a0de483ab5d16667e3ca4bcf30563c80ff1e
Emmanuel Pillay <emmanuelpillaydbn@gmail.com>
2023-11-04 00:20:28 +0200

    add ticket.cs controller

---
+++ WebApplication1/Controllers/Ticket.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class Ticket : ControllerBase
+    {
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Hello World!");
+        }
+
+
+    }
+}
